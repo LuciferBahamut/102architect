409b5fe86b7063243abe5e2fe665bf1f53505112
Quentin TREHEUX <qtreheux@localhost.localdomain>
2019-11-18 18:56:32 +0100

    [MOD] main

--- src/main.c
+++ src/main.c
@@ -9,9 +9,22 @@
 
 int main(int ac, char **av)
 {
-    if (av[1][1] == 'h') {
-        display_help();
-        return (0);
+    if (error_handling(ac, av) == 84)
+        return (84);
+    for (int i = 0; i != ac; i++) {
+        switch (av[i][1]) {
+        case 'h' : display_help();
+            return (0);
+            break;
+        case 't' : return (0);
+            break;
+        case 'z' : return (0);
+            break;
+        case 'r' : return (0);
+            break;
+        case 's' : return (0);
+            break;
+        }
     }
     return (0);
 }
